ae8d8ed51da53ec8c9e7daee38bf454fd628db63
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-01 11:40:03 +0630

    Change class methods to instance methods

--- lib/erdb/adapters/db.rb
+++ lib/erdb/adapters/db.rb
@@ -1,49 +1,56 @@
 module ERDB
   class Db
-    class << self
-      #
-      # Connect to a database.
-      #
-      def connect
-        raise "[connect] Not implemented."
-      end
+    #
+    # @param adapter [String] The adapter to use.
+    # @param database [String] The database to connect to.
+    # @return [void]
+    #
+    def initialize(adapter, database)
+      @adapter = adapter
+      @database = database
+    end
 
-      #
-      # Disconnect from a database.
-      #
-      def disconnect
-        raise "[disconnect] Not implemented."
-      end
+    #
+    # Connect to a database.
+    #
+    def connect
+      raise "[connect] Not implemented."
+    end
 
-      #
-      # Convert database tables to ERD convertable Array.
-      # @return [Array] The converted hash format.
-      # @example The converted hash format:
-      # [
-      #   {
-      #     name: "table_name",
-      #     is_join_table: false,
-      #     columns: ["column_name", "column_name", "column_name"],
-      #     relations: [
-      #       {
-      #         from: {
-      #           table: "table_name",
-      #           column: "column_name"
-      #         },
-      #         to: {
-      #           table: "table_name",
-      #           column: "column_name"
-      #         }
-      #       }
-      #       ...
-      #     ]
-      #   }
-      #   ...
-      # ]
-      #
-      def to_erdb
-        raise "[to_erdb] Not implemented."
-      end
+    #
+    # Disconnect from a database.
+    #
+    def disconnect
+      raise "[disconnect] Not implemented."
+    end
+
+    #
+    # Convert database tables to ERD convertable Array.
+    # @return [Array] The converted hash format. see example result below.
+    #  [
+    #    {
+    #      name: "table_name",
+    #      is_join_table: false,
+    #      columns: ["column_name", "column_name", "column_name"],
+    #      relations: [
+    #        {
+    #          from: {
+    #            table: "table_name",
+    #            column: "column_name"
+    #          },
+    #          to: {
+    #            table: "table_name",
+    #            column: "column_name"
+    #          }
+    #        }
+    #        ...
+    #      ]
+    #    }
+    #    ...
+    #  ]
+    #
+    def to_erdb
+      raise "[to_erdb] Not implemented."
     end
   end
 end
